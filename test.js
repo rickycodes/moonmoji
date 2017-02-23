7bd09d6b5e14be8d3c030fd27bb7b450723c32ad
Ricky Miller <ricky.miller@gmail.com>
2017-02-22 21:44:22 -0500

    :wave: mocha

--- test.js
+++ test.js
@@ -1,44 +1,54 @@
-/* global it describe */
-'use strict'
+const test = require('tape')
+const moonmoji = require('./')
 
-var assert = require('assert')
-var moonmoji = require('./')
+test('no date object argument', function (t) {
+  t.plan(1)
+  const actual = moonmoji()
+  const expected = moonmoji(new Date())
+  t.deepEqual(actual, expected, actual.emoji + '  should return the moon for today')
+})
 
-describe('moonmoji', function () {
-  describe('no date object argument', function () {
-    var actual = moonmoji()
-    var expected = moonmoji(new Date())
-    it(actual.emoji + '  should return the moon for today', function () {
-      assert.deepEqual(actual, expected)
-    })
-  })
+test('🌒  should return correct for waxing crescent', function (t) {
+  t.plan(1)
+  // September 1st, 2014
+  const moon = moonmoji(new Date(2014, 8, 1))
+  t.deepEqual(
+    moon,
+    {
+      name: 'Waxing Crescent',
+      code: ':waxing_crescent_moon:',
+      emoji: '🌒',
+      weight: 6.3825
+    }
+  )
+})
 
-  describe('date object argument', function () {
-    it('🌒  should return correct for waxing crescent', function () {
-      // September 1st, 2014
-      var moon = moonmoji(new Date(2014, 8, 1))
+test('🌔  should return correct for waxing gibbous', function (t) {
+  t.plan(1)
+  // September 7th, 2014
+  const moon = moonmoji(new Date(2014, 8, 7))
+  t.deepEqual(
+    moon,
+    {
+      name: 'Waxing Gibbous',
+      code: ':waxing_gibbous_moon:',
+      emoji: '🌔',
+      weight: 6.3825
+    }
+  )
+})
 
-      assert.deepEqual(moon.name, 'Waxing Crescent')
-      assert.deepEqual(moon.code, ':waxing_crescent_moon:')
-      assert.deepEqual(moon.emoji, '🌒')
-    })
-
-    it('🌔  should return correct for waxing gibbous', function () {
-      // September 7th, 2014
-      var moon = moonmoji(new Date(2014, 8, 7))
-
-      assert.deepEqual(moon.name, 'Waxing Gibbous')
-      assert.deepEqual(moon.code, ':waxing_gibbous_moon:')
-      assert.deepEqual(moon.emoji, '🌔')
-    })
-
-    it('🌘  should return correct for waning crescent', function () {
-      // September 20th, 2014
-      var moon = moonmoji(new Date(2014, 8, 20))
-
-      assert.deepEqual(moon.name, 'Waning Crescent')
-      assert.deepEqual(moon.code, ':waning_crescent_moon:')
-      assert.deepEqual(moon.emoji, '🌘')
-    })
-  })
+test('🌘  should return correct for waning crescent', function (t) {
+  t.plan(1)
+  // September 20th, 2014
+  const moon = moonmoji(new Date(2014, 8, 20))
+  t.deepEqual(
+    moon,
+    {
+      name: 'Waning Crescent',
+      code: ':waning_crescent_moon:',
+      emoji: '🌘',
+      weight: 6.3825
+    }
+  )
 })

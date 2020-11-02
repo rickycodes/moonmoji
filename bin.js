ff356d3f4675a66b6fb237659eea635ef323fcb7
ricky <ricky.miller@gmail.com>
2020-11-01 21:39:27 -0500

    Rust (#38)
    * start
    
    * rust++
    
    * remove travis

---
+++ bin.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+require("./moonmoji")
--- example.js
+++
@@ -1,6 +0,0 @@
-var moon = require('moonmoji')()
-function wat () {
-  return `The moon is currently in its ${moon.name} phase. ${moon.emoji}`
-}
-
-wat()
